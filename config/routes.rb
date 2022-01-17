bd403272cd3eebc1eb916ee7f595cbee770a2591
JANKEE92PL <jankee112@freenet.de>
2022-01-17 14:37:30 +0100

    finish seeds

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,20 @@ Rails.application.routes.draw do
   # verb 'path', to: 'controller#action'
   # ______________________________________
   # read all Tasks
-  get '/tasks', to: 'tasks#index'
-
-  # read one Task
-  get '/tasks/:id', to: 'tasks#show'
-
-  # create a Task
-  get '/tasks/new', to: 'tasks#new' # display the form
-  post '/tasks', to: 'tasks#create'
-
-  # update a Task
-  get '/tasks/:id/edit', to: 'tasks#edit'
-  patch '/tasks/:id', to: 'tasks#update'
-
-  # delete a Task
-  delete '/tasks/:id', to: 'tasks#destroy'
+  # get '/tasks', to: 'tasks#index'
+  #
+  # # read one Task
+  # get '/tasks/:id', to: 'tasks#show'
+  #
+  # # create a Task
+  # get '/tasks/new', to: 'tasks#new' # display the form
+  # post '/tasks', to: 'tasks#create'
+  #
+  # # update a Task
+  # get '/tasks/:id/edit', to: 'tasks#edit'
+  # patch '/tasks/:id', to: 'tasks#update'
+  #
+  # # delete a Task
+  # delete '/tasks/:id', to: 'tasks#destroy'
+  resources :tasks # shortcut für alle methoden!
 end
