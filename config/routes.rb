5416abbe2b1d59bd6cd6089060e945ecb40bc22d
JANKEE92PL <jankee112@freenet.de>
2022-01-17 12:24:52 +0100

    finish routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # CREATE, READ, UPDATE, DELETE
+  # verb 'path', to: 'controller#action'
+  # ______________________________________
+  # read all Tasks
+  get '/tasks', to: 'tasks#index'
+
+  # read one Task
+  get '/tasks/:id', to: 'tasks#show'
+
+  # create a Task
+  get '/tasks/new', to: 'tasks#new' # display the form
+  post '/tasks', to: 'tasks#create'
+
+  # update a Task
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # delete a Task
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
